cb8a0f2badf0576fc5bfbbc0e0c3d065e26bf982
Jason Kuo <jkuo@jcvi.org>
2015-06-24 11:00:11 -0400

    fixed bug: some pages have an additional line after protein name

--- protein.py
+++ protein.py
@@ -33,17 +33,24 @@ def openurl(url):
 #Parses a page from Uniprot to obtain basic information about a protein
 #Returns the information in JSON form
 def textToInfo(text):
+    lineNum = 0
     lines = text.split("\n")
-    tempLine = lines[0]
+    tempLine = lines[lineNum]
     identifier = tempLine[5:17]
     #print "Identifier: " + identifier
-    tempLine = lines[5]
+    lineNum+=5
+    tempLine = lines[lineNum]
     name = tempLine[string.find(tempLine, "Full=", 0, len(tempLine))+5:string.find(tempLine, " {", 0, len(tempLine))]
     #print "Protein Name: " + name
-    tempLine = lines[6]
+    lineNum+=1
+    if lines[lineNum][5] == " ":
+        print "in if"
+        lineNum+=1
+    tempLine = lines[lineNum]
     gene = tempLine[string.find(tempLine, "Name=", 0, len(tempLine))+5:string.find(tempLine, " {", 0, len(tempLine))]
     #print "Gene: " + gene
-    organism = lines[9][5:len(lines[9])-1]
-    #print "Organism: " + organism
-    info = {"Identifier": identifier, "Protein_Name": name, "Gene": gene, "Organism": organism}
+    lineNum+=3
+    organisms = lines[lineNum][5:len(lines[lineNum])-1]
+    #print "Organisms: " + organisms
+    info = {"Identifier": identifier, "Protein_Name": name, "Gene": gene, "Organisms": organisms}
     return json.dumps(info)
