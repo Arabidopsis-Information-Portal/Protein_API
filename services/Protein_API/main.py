dde3f701310ecd7fc229bdf84309d5ab5beb13c1
Jason Kuo <jkuo@jcvi.org>
2015-08-10 13:29:10 -0400

    adding add_view

--- services/Protein_API/main.py
+++ services/Protein_API/main.py
@@ -3,12 +3,11 @@ from intermine.webservice import Service
 
 #query thalemine, the database used to obtain the output information
 service = Service("https://apps.araport.org:443/thalemine/service")
-#query out of the proteins
-query = service.new_query("Protein")
-#adds these fields to the query
+#add these columns to the query
 query.add_view(
-    "primaryIdentifier", "secondaryIdentifier", "mRNA.primaryIdentifier",
-    "name", "uniprotName", "length", "ecNumber", "id", "isFragment", "isUniprotCanonical", "molecularWeight", "primaryAccession", "symbol", "uniprotAccession"
+    "isFragment", "isUniprotCanonical", "length", "molecularWeight", "name",
+    "primaryAccession", "primaryIdentifier", "uniprotName", "uniprotAccession",
+    "mRNA.primaryIdentifier"
 )
 
 #a list of all possible outputs also listed in the metadata
@@ -29,99 +28,88 @@ outputs = [
     "uniprotAccession"
 ]
 
+displayOutputs = [
+    "Primary_Identifier",
+    "Secondary_Identifier",
+    "mRNA_Primary_Identifier",
+    "Name",
+    "Uniprot_Name",
+    "Length",
+    "EC_Number",
+    "ID",
+    "Is_Fragment",
+    "Is_Uniprot_Canonical",
+    "Molecular_Weight",
+    "Primary_Accession",
+    "Symbol",
+    "Uniprot_Accession"
+]
+
 #main search function
 def search(parameters):
     if "Identifier" in parameters.keys():
         identifierInput = parameters["Identifier"]
-        print getProtein(identifierInput, parameters["Output"])
-
-#print all names when input is empty
-#(deprecated with the removal of the begin and end parameters)
-def noInput(parameters):
-    begin = 0
-    end = -1
-    #assume user begins counting with 1
-    if "Begin" in parameters.keys():
-        begin = int(parameters["Begin"]) - 1
-    else:
-        begin = 0
-    if "End" in parameters.keys():
-        end = int(parameters["End"]) - 1
-    else:
-        end = -1
-    print getAllIdentifiers(begin, end)
-
-#returns a JSON representing a list of all protein identifiers from begin to end
-#(deprecated with the removal of the begin and end parameters)
-def getAllIdentifiersFromBeginToEnd(begin, end):
-    entries = query.rows()
-    if end == -1:
-        end = len(entries)
-    if begin > end:
-        raise Exception("Begin is greater than End")
-    identifiers = []
-    i = -1
-    for row in entries:
-        i+=1
-        if i < begin:
-            continue
-        if i >= end:
-            break
-        identifiers.append(row["mRNA.primaryIdentifier"]) #the identifier parameter uses the mRNA primary identifier format
-
-    #remove duplicate values
-    last = "placeholder"
-    noDupes = []
-    for identifier in identifiers:
-        if identifier == last:
-            continue
-        noDupes.append(identifier)
-        last = identifier
-
-    return json.dumps(noDupes)
+        getProtein(identifierInput, "all")
 
 #returns a JSON representing a list of all protein identifiers
 def getAllIdentifiers():
+    #query out of the proteins
+    query = service.new_query("Protein")
+    #adds these fields to the query
+    query.add_view("mRNA.primaryIdentifier","name")
     #put the list of the data from the query into entries
     entries = query.rows()
     #remove duplicate identifiers from the query
     entries = removeDuplicates(entries, "mRNA.primaryIdentifier")
     #initialize the array to append in the loop
-    identifiers = []
+    protein = {}
     for row in entries:
         #append the identifier in each row to the list of identifiers
-        identifiers.append(row["mRNA.primaryIdentifier"])
+        protein["mRNA_Primary_Identifier"] = row["mRNA.primaryIdentifier"]
+        protein["Name"] = row["name"]
+        print json.dumps(protein)
+        print "---"
     #json.dumps(parameter) is a function that converts the parameter into JSON format
-    return json.dumps(identifiers)
+    #return json.dumps(identifiers)
 
 #returns info about a protein given an identifier
 def getProtein(identifier, info):
+    #query out of the proteins
+    query = service.new_query("Protein")
+    #adds these fields to the query
+    query.add_view(
+        "primaryIdentifier", "secondaryIdentifier", "mRNA.primaryIdentifier",
+        "name", "uniprotName", "length", "ecNumber", "id", "isFragment", "isUniprotCanonical", "molecularWeight", "primaryAccession", "symbol", "uniprotAccession"
+    )
+    query.add_constraint("mRNA.primaryIdentifier", "=", identifier, code = "A")
     entries = []
-    foundOne = False
+#    foundOne = False
     protein = []
-    #find all versions of the protein in the query
     for row in query.rows():
+        entries.append(row)
+    #find all versions of the protein in the query
+#    for row in query.rows():
         #if I am currently on a row that does not match but i have already found a protein,
         #stop looking because I have already found them all
         #This works because all the versions are grouped together in order so if
         #I am currently on a row and I have already found some versions,
         #I have already iterated past all of them
-        if row["mRNA.primaryIdentifier"] != identifier and foundOne == True:
-            break
+#        if row["mRNA.primaryIdentifier"] != identifier and foundOne == True:
+#            break
         #add rows matching the identifier
-        if row["mRNA.primaryIdentifier"] == identifier:
-            entries.append(row)
-            foundOne = True
+#        if row["mRNA.primaryIdentifier"] == identifier:
+#            entries.append(row)
+#            foundOne = True
 
     #in case the protein is not found with the identifier
-    if entries == []:
-        raise Exception("Protein not found")
+#    if entries == []:
+#        raise Exception("Protein not found")
 
     #remove duplicates of the wanted information unless all information is wanted
     if info == "all":
         noDupes = entries
-    else:
-        noDupes = removeDuplicates(entries, info)
+#    else:
+#        noDupes = removeDuplicates(entries, info)
 
     #do different things for getting all information and getting a single output
     if info == "all":
@@ -129,32 +117,31 @@ def getProtein(identifier, info):
             i = 0
             #initialize the dictionary to be used in the loop
             version = {}
+            #there are multiple versions of a protein due to a temporary database bug (e.g AT2G46830.1)
+            #where separate entities incorrectly share a single identifier.
+            #The problem is inherited from UniProt and it is expected to go away in future versions
             while i < len(outputs):
                 #using each of the outputs as a key, set the value in the version dictionary
                 #to be the value depending on entry
-                version[outputs[i]] = entry[outputs[i]]
+                version[displayOutputs[i]] = entry[outputs[i]]
                 i+=1
             #add this version to the list of proteins
-            protein.append(version)
-        #if there is only one protein, only return the protein as a single key-value pair
-        #this removes a pair of unnecessary brackets in the final JSON
-        if len(protein) == 1:
-            protein = protein[0]
-        return json.dumps(protein)
+            print json.dumps(version)
+            print "---"
     else:
         for entry in noDupes:
             #for each entry, retrieve the wanted information
             infoValue = entry[info]
             #add the information as a key-value pair to the protein list
             i = {info: infoValue}
-            protein.append(i)
-            #if there is only one protein, only return the protein as a single key-value pair
-            #this removes a pair of unnecessary brackets in the final JSON
-        if len(protein) == 1:
-            protein = protein[0]
-        return json.dumps(protein)
+            print json.dumps(i)
+            print "---"
 
 #removes the entries with duplicates of the given property from the given list
+#this function will mostly be used to remove identifier duplicates
+#duplicates occur because ThaleMine creates multiple entries for the same protein if there is more than one piece of information
+#e.g. if I had a query of proteins where I wanted the name of the authors who published about the protein,
+#there would be many entries for AT1G01010.1 since many authors contributed to publication about the protein
 def removeDuplicates(list, propertyName):
     last = "placeholder"
     #initialize the array to append in the loop
@@ -172,45 +159,6 @@ def removeDuplicates(list, propertyName):
         last = entry[propertyName]
     return noDupes
 
-
-#returns info about all proteins in a given list of identifiers
-#(deprecated so multiple identifiers are no longer allowed)
-def getProteins(identifierList, info):
-    proteinList = []
-    for identifier in identifierList:
-        entries = []
-        foundOne = False
-        protein = []
-        #find all versions of the protein
-        for row in query.rows():
-            if row["primaryIdentifier"] != identifier and foundOne == True:
-                break
-            if row["primaryIdentifier"] == identifier:
-                entries.append(row)
-                foundOne = True
-
-        #in case the protein is not found with the identifier
-        if entries == []:
-            raise Exception("One or more proteins were not found")
-
-        #remove duplicate values
-        last = "placeholder"
-        noDupes = []
-        for entry in entries:
-            if entry[info] == last:
-                continue
-            noDupes.append(entry)
-            last = entry[info]
-
-        #get information
-        for entry in noDupes:
-            infoValue = entry[info]
-            protein.append({"primaryIdentifier": entry["primaryIdentifier"], info: infoValue})
-        proteinList.append(protein)
-    return json.dumps(proteinList)
-
-
-
 #list function (in development)
 def list(parameters):
-    print getAllIdentifiers()
+    getAllIdentifiers()
