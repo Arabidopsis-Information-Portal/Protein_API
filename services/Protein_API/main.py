262c683e9ccc828fc6a82e5bd5445c135e12822e
Jason Kuo <jkuo@jcvi.org>
2015-08-10 15:36:26 -0400

    cleaning up

--- services/Protein_API/main.py
+++ services/Protein_API/main.py
@@ -7,35 +7,29 @@ service = Service("https://apps.araport.org:443/thalemine/service")
 #a list of all possible outputs also listed in the metadata
 outputs = [
     "primaryIdentifier",
-    "secondaryIdentifier",
     "mRNA.primaryIdentifier",
     "name",
     "uniprotName",
     "length",
-    "ecNumber",
     "id",
     "isFragment",
     "isUniprotCanonical",
     "molecularWeight",
     "primaryAccession",
-    "symbol",
     "uniprotAccession"
 ]
 
 displayOutputs = [
     "Primary_Identifier",
-    "Secondary_Identifier",
     "mRNA_Primary_Identifier",
     "Name",
     "Uniprot_Name",
     "Length",
-    "EC_Number",
     "ID",
     "Is_Fragment",
     "Is_Uniprot_Canonical",
     "Molecular_Weight",
     "Primary_Accession",
-    "Symbol",
     "Uniprot_Accession"
 ]
 
@@ -61,10 +55,9 @@ def getAllIdentifiers():
         #append the identifier in each row to the list of identifiers
         protein["mRNA_Primary_Identifier"] = row["mRNA.primaryIdentifier"]
         protein["Name"] = row["name"]
+        #json.dumps(parameter) is a function that converts the parameter into JSON format
         print json.dumps(protein)
         print "---"
-    #json.dumps(parameter) is a function that converts the parameter into JSON format
-    #return json.dumps(identifiers)
 
 #returns info about a protein given an identifier
 def getProtein(identifier, info):
@@ -72,64 +65,31 @@ def getProtein(identifier, info):
     query = service.new_query("Protein")
     #adds these fields to the query
     query.add_view(
-        "primaryIdentifier", "secondaryIdentifier", "mRNA.primaryIdentifier",
-        "name", "uniprotName", "length", "ecNumber", "id", "isFragment", "isUniprotCanonical", "molecularWeight", "primaryAccession", "symbol", "uniprotAccession"
+        "primaryIdentifier", "mRNA.primaryIdentifier", "name", "uniprotName", "length", "id", "isFragment",
+        "isUniprotCanonical", "molecularWeight", "primaryAccession", "uniprotAccession"
     )
     query.add_constraint("mRNA.primaryIdentifier", "=", identifier, code = "A")
     entries = []
-#    foundOne = False
     protein = []
     for row in query.rows():
         entries.append(row)
-    #find all versions of the protein in the query
-#    for row in query.rows():
-        #if I am currently on a row that does not match but i have already found a protein,
-        #stop looking because I have already found them all
-        #This works because all the versions are grouped together in order so if
-        #I am currently on a row and I have already found some versions,
-        #I have already iterated past all of them
-#        if row["mRNA.primaryIdentifier"] != identifier and foundOne == True:
-#            break
-        #add rows matching the identifier
-#        if row["mRNA.primaryIdentifier"] == identifier:
-#            entries.append(row)
-#            foundOne = True
 
-    #in case the protein is not found with the identifier
-#    if entries == []:
-#        raise Exception("Protein not found")
-
-    #remove duplicates of the wanted information unless all information is wanted
-    if info == "all":
-        noDupes = entries
-#    else:
-#        noDupes = removeDuplicates(entries, info)
-
-    #do different things for getting all information and getting a single output
-    if info == "all":
-        for entry in noDupes:
-            i = 0
-            #initialize the dictionary to be used in the loop
-            version = {}
-            #there are multiple versions of a protein due to a temporary database bug (e.g AT2G46830.1)
-            #where separate entities incorrectly share a single identifier.
-            #The problem is inherited from UniProt and it is expected to go away in future versions
-            while i < len(outputs):
-                #using each of the outputs as a key, set the value in the version dictionary
-                #to be the value depending on entry
-                version[displayOutputs[i]] = entry[outputs[i]]
-                i+=1
-            #add this version to the list of proteins
-            print json.dumps(version)
-            print "---"
-    else:
-        for entry in noDupes:
-            #for each entry, retrieve the wanted information
-            infoValue = entry[info]
-            #add the information as a key-value pair to the protein list
-            i = {info: infoValue}
-            print json.dumps(i)
-            print "---"
+    for entry in entries:
+        i = 0
+        #initialize the dictionary to be used in the loop
+        version = {}
+        #there are multiple versions of a protein due to a temporary database bug (e.g AT2G46830.1)
+        #where separate entities incorrectly share a single identifier.
+        #The problem is inherited from UniProt and it is expected to go away in future versions
+        while i < len(outputs):
+            #using each of the outputs as a key, set the value in the version dictionary
+            #to be the value depending on entry
+            version[displayOutputs[i]] = entry[outputs[i]]
+            i+=1
+        #add this version to the list of proteins
+        print json.dumps(version)
+        #when --- is printed between two objects, Adama interprets the two objects as two different results
+        print "---"
 
 #removes the entries with duplicates of the given property from the given list
 #this function will mostly be used to remove identifier duplicates
